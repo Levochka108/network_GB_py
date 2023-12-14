8c45c7f67246fa542f3595325c3ce44874900cc7
дмитрий клочков <d.klochkov9421@gmail.com>
2023-12-15 00:03:56 +0300

    добавленно логирование

--- client.py
+++ client.py
@@ -1,5 +1,15 @@
 import socket
 import threading
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s [%(levelname)s] %(message)s',
+                    handlers=[
+                        logging.FileHandler("logs/client.log"),
+                        logging.StreamHandler()
+                    ])
+logger = logging.getLogger(__name__)
 
 # Choosing Nickname
 nickname = input("Choose your nickname: ")
@@ -19,20 +29,28 @@ def receive():
                 client.send(nickname.encode('ascii'))
             else:
                 print(message)
-        except:
+                logger.info(message)
+        except Exception as e:
             # Close Connection When Error
-            print("An error occured!")
+            logger.error(f"An error occurred: {e}")
             client.close()
             break
 
 def write():
     while True:
-        message = '{}: {}'.format(nickname, input(''))
-        client.send(message.encode('ascii'))
+        try:
+            message = '{}: {}'.format(nickname, input(''))
+            client.send(message.encode('ascii'))
+        except Exception as e:
+            logger.error(f"An error occurred while sending message: {e}")
 
 # Starting Threads For Listening And Writing
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
+if __name__ == '__main__':
+    try:
+        receive_thread = threading.Thread(target=receive)
+        receive_thread.start()
 
-write_thread = threading.Thread(target=write)
-write_thread.start()
+        write_thread = threading.Thread(target=write)
+        write_thread.start()
+    except Exception as e:
+        logger.error(f"An error occurred while starting threads: {e}")
--- server.py
+++ server.py
@@ -1,14 +1,25 @@
-#!/bin/python3
 import socket
 import threading
+import logging
 
 # Connection Data
-host = '127.0.0.1'
-port = 55555
+HOST = '127.0.0.1'
+PORT = 55555
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s [%(levelname)s] %(message)s',
+                    handlers=[
+                        logging.FileHandler("/logs/server.log"),
+                        logging.StreamHandler()
+                    ])
+
+logger = logging.getLogger(__name__)
+
 
 # Starting Server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
+server.bind((HOST, PORT))
 server.listen()
 
 # Lists For Clients and Their Nicknames
@@ -18,7 +29,10 @@ nicknames = []
 # Sending Messages To All Connected Clients
 def broadcast(message):
     for client in clients:
-        client.send(message)
+        try:
+            client.send(message)
+        except Exception as e:
+            logger.error(f"An error occurred while broadcasting message: {e}")
 
 # Handling Messages From Clients
 def handle(client):
@@ -27,8 +41,9 @@ def handle(client):
             # Broadcasting Messages
             message = client.recv(1024)
             broadcast(message)
-        except:
+        except Exception as e:
             # Removing And Closing Clients
+            logger.error(f"An error occurred while handling client: {e}")
             index = clients.index(client)
             clients.remove(client)
             client.close()
@@ -40,24 +55,31 @@ def handle(client):
 # Receiving / Listening Function
 def receive():
     while True:
-        # Accept Connection
-        client, address = server.accept()
-        print("Connected with {}".format(str(address)))
+        try:
+            # Accept Connection
+            client, address = server.accept()
+            logger.info("Connected with {}".format(str(address)))
 
-        # Request And Store Nickname
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
-        nicknames.append(nickname)
-        clients.append(client)
+            # Request And Store Nicknames
+            client.send('NICK'.encode('ascii'))
+            nickname = client.recv(1024).decode('ascii')
+            nicknames.append(nickname)
+            clients.append(client)
 
-        # Print And Broadcast Nickname
-        print("Nickname is {}".format(nickname))
-        broadcast("{} joined!".format(nickname).encode('ascii'))
-        client.send('Connected to server!'.encode('ascii'))
+            # Print And Broadcast Nickname
+            logger.info("Nickname is {}".format(nickname))
+            broadcast("{} joined!".format(nickname).encode('ascii'))
+            client.send('Connected to server!'.encode('ascii'))
 
-        # Start Handling Thread For Client
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.start()
+            # Start Handling Thread For Client
+            thread = threading.Thread(target=handle, args=(client,))
+            thread.start()
+        except Exception as e:
+            logger.error(f"An error occurred while accepting connection: {e}")
 
-print("Server if listening...")
-receive()
+if __name__ == "__main__":
+    try:
+        logger.info("Server is listening...")
+        receive()
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
